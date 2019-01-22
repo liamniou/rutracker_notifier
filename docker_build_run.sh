0b668f89bc4e5ca52ee928fb391b83a30be1a6bf
Stanislau Liamniou <stanislau.liamniou@netcracker.com>
2019-01-22 15:16:16 +0300

    Add conditions for generation of config file and copy of db file

--- docker_build_run.sh
+++ docker_build_run.sh
@@ -12,23 +12,40 @@ apt-get update
 apt-get install docker-ce
 
 # Prepare config.py
-echo -n "Enter telegram API token and press [ENTER]: "
-read token
-echo -n "Enter username to access https://rutracker.org and press [ENTER]: "
-read rutracker_username
-echo -n "Enter password for ${rutracker_username} and press [ENTER]: "
-read rutracker_password
-echo -n "Enter path to DB file with data if you have any. Otherwise just press [ENTER]: "
-read db_path
-sed -i "s/ENTER_TOKEN/${token}/g" ./app/config.py
-sed -i "s/ENTER_rutracker_login/${rutracker_username}/g" ./app/config.py
-sed -i "s/ENTER_rutracker_pass/${rutracker_password}/g" ./app/config.py
-sed -i "s:rutracker_notifier.db:/mnt/rutracker_notifier.db:g" ./app/config.py
+echo -n "Generate config.py? [y/n]: "
+read generate_config
+if [[ ${generate_config,,} == "y" || ${generate_config,,} == "yes" ]]; then
+    echo -n "Enter telegram API token and press [ENTER]: "
+    read token
+    echo -n "Enter username to access https://rutracker.org and press [ENTER]: "
+    read rutracker_username
+    echo -n "Enter password for ${rutracker_username} and press [ENTER]: "
+    read rutracker_password
+    cat > ./app/config.py << EOL
+token = "${token}"
+database_name = "/mnt/rutracker_notifier.db"
+rutracker_login = "${rutracker_username}"
+rutracker_password = "${rutracker_password}"
+EOL
+fi
 
-if [[ -z $db_path ]]; then
-    mv ./app/rutracker_notifier.db ~/rutracker_notifier_bot_app/
-else
+db_path='./app/rutracker_notifier.db'
+if [[ ! -f ~/rutracker_notifier_bot_app/rutracker_notifier.db ]]; then
+    echo -n "Enter path to DB file with data if you have any. To use default (${db_path}) just press [ENTER]: "
+    read user_db_path
+    if [[ -n $user_db_path ]]; then
+        db_path=$user_db_path
+    fi
     cp $db_path ~/rutracker_notifier_bot_app/rutracker_notifier.db
+else
+    echo -n "~/rutracker_notifier_bot_app contains file rutracker_notifier.db. Replace it? [y/n]: "
+    read replace_db_file
+    if [[ ${replace_db_file,,} == "y" || ${replace_db_file,,} == "yes" ]]; then
+        echo -n "Enter path to DB file with data and press [ENTER]: "
+        read user_db_path
+        db_path=$user_db_path
+        cp $db_path ~/rutracker_notifier_bot_app/rutracker_notifier.db
+    fi
 fi
 
 # Build image
